0c3ae47c82cff0bfa8556be3def23a1b34087e2e
xstarcd <xstarcd@users.noreply.github.com>
2015-06-30 10:36:34 +0800

    评论key

--- wiki/scripts/vimwiki.js
+++ wiki/scripts/vimwiki.js
@@ -19,7 +19,7 @@ var kwiki = {
         if ($('#no-comment').length > 0) return;
         var thread, extraCSS, theJS;
 
-        var kwiki_identifier = 'wiki' + window.location.pathname.replace(/\//g, '_').replace('index.html', '').replace(/__/g,'_')
+        var kwiki_identifier = 'wiki' + window.location.pathname.replace(/\//g, '_').replace('index.html', '').replace(/__/g,'_');
             //.replace('.html', '');
         var kwiki_title = $(document).attr("title");
         //var kwiki_url = 'http://w.gdu.me' + window.location.pathname.replace('index.html', '');
